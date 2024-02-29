8f877683a01c9b5a1d19934ccd46eacfe6943980
IzanGJ <izan.galan.juncar@ieselcalamot.com>
2024-02-29 17:37:14 +0100

    Creacion

---
+++ src/main/java/com/mycompany/codedynamix/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.codedynamix;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
