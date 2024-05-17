8d592d9686f866d2ef91d87d889d0f2a83f4d6d4
David-Romero31 <david.romero.piqueras@ieselcalamot.com>
2024-05-17 18:47:13 +0200

    Añadido allProducts() al DAO  (aún sin probar)

--- src/main/java/persistence/codeDynamixDAO.java
+++ src/main/java/persistence/codeDynamixDAO.java
@@ -35,6 +35,7 @@ public class codeDynamixDAO {
         return companies;
     }
     
+    
     public void insertCompany(CompanyObj p) throws SQLException {
         Connection c = conectar();
         PreparedStatement ps = c.prepareStatement("insert into company values (?,?);");
@@ -44,6 +45,91 @@ public class codeDynamixDAO {
         ps.close();
         desconectar(c);
     }
+    
+    
+    
+    
+    public HashMap<Integer, ProductObj> allProducts() throws SQLException {
+        Connection c = conectar();
+        HashMap<Integer, ProductObj> products = new HashMap<>();
+        String query = "SELECT product.*, keyboard.type AS kb_type, keyboard.language AS kb_language, keyboard.lenght AS kb_lenght, keyboard.wireless AS kb_wireless, chair.backrest AS ch_backrest, chair.	wheels AS ch_wheels, chair.armrest AS ch_armrest, mouse.handDexterity AS ms_handDexterity, mouse.lateralButtons AS ms_lateralButtons, mouse.wireless AS ms_wireless, taula.wheels AS tb_wheels, taula.legs AS tb_legs, taula.adjutableHeight AS tb_djutableHeight, taula.material AS tb_material from product\n" +
+                        "JOIN keyboard ON keyboard.ID = product.ID\n" +
+                        "JOIN chair ON chair.ID = product.ID\n" +
+                        "JOIN mouse ON mouse.ID = product.ID\n" +
+                        "JOIN taula ON taula.ID = product.ID\n" +
+                        "GROUP BY product.ID;";
+        Statement st = c.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        while (rs.next()) {
+            int id = rs.getInt("ID");
+            String tipo = rs.getString("type");
+            try {
+                switch (tipo) {
+                    case "table":
+                        boolean alturaAjustable;
+                        if (rs.getInt("tb_adjustableHeight") == 1) {
+                            alturaAjustable = true;
+                        } else {
+                            alturaAjustable = false;
+                        }
+                        products.put(id, new ErgonomicTable(rs.getInt("tb_wheels"), rs.getInt("tb_legs"), alturaAjustable, rs.getString("tb_material"), rs.getString("name"), id, rs.getString("description"), rs.getInt("weight"), rs.getInt("color")));
+                        break;
+                        
+                    case "chair":
+                        boolean respaldo;
+                        boolean reposabrazos;
+                        if (rs.getInt("ch_backrest") == 1) {
+                            respaldo = true;
+                        } else {
+                            respaldo = false;
+                        }
+                        
+                        if (rs.getInt("ch_armrest") == 1) {
+                            reposabrazos = true;
+                        } else {
+                            reposabrazos = false;
+                        }
+                        products.put(id, new ErgonomicChair(respaldo,rs.getInt("ch_wheels"),reposabrazos, rs.getString("name"), id, rs.getString("description"), rs.getInt("weight"), rs.getInt("color")));
+                        break;
+                    
+                    case "keyboard":
+                        boolean wireless;
+                        if (rs.getInt("kb_wireless") == 1) {
+                            wireless = true;
+                        } else {
+                            wireless = false;
+                        }
+                        products.put(id, new ErgonomicKeyboard(rs.getString("tb_type"), rs.getString("tb_language"), rs.getInt("tb_lenght"), wireless, rs.getString("name"), id, rs.getString("description"), rs.getInt("weight"), rs.getInt("color")));
+                        break;
+                        
+                    case "mouse":
+                        boolean handDexterity;
+                        if (rs.getInt("ch_backrest") == 1) {
+                            handDexterity = true;
+                        } else {
+                            handDexterity = false;
+                        }
+                        
+                        if (rs.getInt("ch_armrest") == 1) {
+                            wireless = true;
+                        } else {
+                            wireless = false;
+                        }
+                        products.put(id, new ErgonomicMouse(handDexterity, rs.getInt("ms_lateralButtons"), wireless, rs.getString("name"), id, rs.getString("description"), rs.getInt("weight"), rs.getInt("color")));
+                        break;
+                }
+                
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        rs.close();
+        st.close();
+        desconectar(c);
+        return products;
+    }
+    
+    
     
     
     public void insertTable(ErgonomicTable t) throws SQLException {
@@ -85,7 +171,7 @@ public class codeDynamixDAO {
         ps.setFloat(4, ch.getPrice());
         ps.setInt(5, ch.getWeight());
         ps.setString(6, String.valueOf(ch.getColor()));
-        ps.setString(7, "CHAIR");
+        ps.setString(7, "chair");
         ps.executeUpdate();
         ps.close();
         
